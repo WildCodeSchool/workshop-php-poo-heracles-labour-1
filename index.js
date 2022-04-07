d8acbd0cb54fe1935ad66790501f680ebe657888
DAMIEN DUPONT <damiendupont.dd@gmail.com>
2022-04-07 13:49:57 +0200

    creation boucle fight 1/x

--- index.js
+++ index.js
@@ -1,4 +1,3 @@
-// import { Fighter } from "./src/fighter";
 const Fighter = require('./src/fighter');
 
 // création de deux instances de combattants
@@ -6,6 +5,12 @@ const heracles = new Fighter('Héraclès', 20, 6, '🧔', '💚');
 const lion = new Fighter('Lion de Némée', 11, 13, '🦁', '💙');
 
 // fight !
-while (heracles.life > 0 && lion.life > 0) {}
+//while (heracles.life > 0 && lion.life > 0) {}
+for (round = 1; (heracles.isAlive && lion.isAlive); round++)
+console.log(`round ${round}`);
 heracles.fight(lion);
-lion.fight(heracles)
+lion.fight(heracles);
+
+
+
+//  return `${this.name} ${this.emoji}⚔️${enemy.emoji} ${enemy.name} => ${enemy.emoji}${enemy.heart} ${enemy.life}`;
