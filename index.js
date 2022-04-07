b50e4091d1ad12a7e80315688220d159681a4209
DAMIEN DUPONT <damiendupont.dd@gmail.com>
2022-04-07 13:33:59 +0200

    creation method fight 1/x

--- index.js
+++ index.js
@@ -2,7 +2,8 @@
 const Fighter = require('./src/fighter');
 
 // création de deux instances de combattants
-const heracles = new Fighter('🧔 Héraclès', 20, 6);
-const lion = new Fighter('🦁 Lion de Némée', 11, 13);
+const heracles = new Fighter('Héraclès', 20, 6, '🧔', '💚');
+const lion = new Fighter('Lion de Némée', 11, 13, '🦁', '💙');
 
 // fight !
+heracles.fight(lion)
--- src/fighter.js
+++ src/fighter.js
@@ -1,15 +1,21 @@
 const MAX_LIFE = 100;
 
 class Fighter {
-    constructor(name, strength, dexterity) {
+    constructor(name, strength, dexterity, emoji, heart='💜') {
         this.name = name;
+        this.emoji = emoji;
         this.strength = strength;
         this.dexterity = dexterity;
+        this.heart = heart;
         this.life = MAX_LIFE;
     }
-    fight(){
-        // pinaaage!
+    fight(enemy){
+        let hit = Math.trunc(Math.random()*this.strength);
+        let wound = hit - this.dexterity;
+        wound < 0 ? '' : enemy.life - wound < 0 ? enemy.life = 0 : enemy.life = enemy.life - wound;
+        console.log(`${this.name} ${this.emoji}⚔️${enemy.emoji} ${enemy.name} => ${enemy.emoji}${enemy.heart} ${enemy.life}`);
     }
 }
+
 module.exports = Fighter;
-// export {Fighter}
+// export {Fighter} //
