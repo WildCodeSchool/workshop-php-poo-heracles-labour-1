a34d5aea035134a55c0259dae85d4674e06aa9a5
DAMIEN DUPONT <damiendupont.dd@gmail.com>
2022-04-07 22:54:28 +0200

    corrections finales, nettoyage de code, version optimisée

--- index.js
+++ index.js
@@ -2,20 +2,47 @@ const Fighter = require('./src/fighter');
 
 // création de deux instances de combattants
 const heracles = new Fighter('Héraclès', 20, 6, '🧔', '💚');
-const lion = new Fighter('Lion de Némée', 11, 13, '🦁',);
+const lion = new Fighter('Lion de Némée', 11, 13, '🦁');
 
+/**
+ * Helper to produce the result of a round
+ */
+const roundDisplay = (attacker, enemy) => {
+    return enemy.wound === 0 ?
+    `${attacker.name} ${attacker.emoji}🗡️${enemy.emoji} ${enemy.name} => ${enemy.emoji}🛡️`
+    :
+    `${attacker.name} ${attacker.emoji}🗡️${enemy.emoji} ${enemy.name} => ${enemy.emoji}:` + (enemy.life > 0 ? ` ${enemy.life}${enemy.heart}` : `💀`);
+};
 
-console.log(heracles.fight(lion));
-console.log(lion.fight(heracles));
-/*
-// fight !
-//while (heracles.life > 0 && lion.life > 0) {}
-for (round = 1; (heracles.isAlive && lion.isAlive); round++)
-console.log(`round ${round}`);
-console.log(heracles.fight(lion));
-console.log(lion.fight(heracles));
+/**
+ * Helper to dispatch fighters between winner and loser
+ */
+const score = (attacker, enemy) => {
+    return attacker.isAlive() ? {
+        winner: attacker,
+        loser: enemy,
+    } : {
+        winner: enemy,
+        loser: attacker
+    };
+};
 
-*/
+let round = 1;
 
-// 🏆 et le perdant 💀
-//  return `${this.name} ${this.emoji}⚔️${enemy.emoji} ${enemy.name} => ${enemy.emoji}${enemy.heart} ${enemy.life}`;
+while (heracles.isAlive() && lion.isAlive()) {
+    console.log(`⏳ Round #${round}`);
+
+    heracles.fight(lion);
+    console.log(roundDisplay(heracles, lion));
+    if (lion.life>0) {
+        lion.fight(heracles);
+        console.log(roundDisplay(lion, heracles));
+    }
+
+    round++;
+}
+
+const result = score(heracles, lion);
+
+console.log(`💀 ${result.loser.name} est mort`);
+console.log(`🏆 ${result.winner.name} gagne (Reste ${result.winner.life}${result.winner.heart})`);
--- src/fighter.js
+++ src/fighter.js
@@ -8,18 +8,21 @@ class Fighter {
         this.dexterity = dexterity;
         this.heart = heart;
         this.life = MAX_LIFE;
+        this.wound = 0;
     }
     getRandomInt(max){
         return 1 + Math.floor(Math.random() * max);
     }
 
     fight(enemy){
-        const hit = this.getRandomInt(this.strength);
+      const hit = this.getRandomInt(this.strength);
       // const wound = hit - this.dexterity;
-      const wound = Math.max(hit - enemy.dexterity, 0);
+      enemy.wound = Math.max(hit - enemy.dexterity, 0);
+      enemy.life = Math.max(enemy.life - enemy.wound, 0);
+      // let result = (wound === 0 ? `${this.emoji}🛡️` : (enemy.isAlive ? `Victoire de ${this.name}: 💀${enemy.emoji}` :`${enemy.emoji}${enemy.heart}: ↘${enemy.life}`));
       //  wound < 0 ? '' : enemy.life - wound < 0 ? enemy.life = 0 : enemy.life = enemy.life - wound;
-      enemy.life = Math.max(enemy.life - wound, 0);
-      return `${this.name} ${this.emoji}🗡️${enemy.emoji} ${enemy.name} => ${enemy.emoji}${enemy.heart} ${enemy.life}`
+      
+      // return `${result}`
     }
 
     isAlive() {
